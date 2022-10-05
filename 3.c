49d8c929a7d5119fdce7ebff86a850f07ede4a63
sahil dahake <sahildahake1999@gmail.com>
2022-10-05 19:34:33 +0530

    commit

--- 3.c
+++ 3.c
@@ -1,15 +1,14 @@
-//arithmatic operator//
 #include<stdio.h>
 int main(int argc, char const *argv[])
 {
     int a,b;
     a=36;
     b=6;
-    printf("a+b = %d\n", a+b);
-    printf("a-b = %d\n", a-b);
-    printf("a*b = %d\n", a*b);
-    printf("a/b = %d\n", a/b);
-    printf("a%b = %d\n", a%b);
+    printf("a+b= %d\n" ,a+b);
+    printf("a-b=%d\n", a-b);
+    printf("a*b=%d\n", a*b);
+    printf("a/b=%d\n" , a/b);
+    printf("a%b=%d\n",a%b);
     return 0;
 }
 
