1d3e4ebcbe9c8ecc61797d2f6dfd1f54c000e08a
sahil dahake <sahildahake1999@gmail.com>
2022-10-06 22:02:12 +0530

    commit

---
+++ 5.c
@@ -0,0 +1,9 @@
+//format specifier//
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a=5;
+    float b=4.5;
+    printf("the value of a is %d and the value of b is %12.5f", a,b);
+    return 0;
+}
