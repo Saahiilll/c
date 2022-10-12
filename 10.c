a79cd4383386dca617e3947110f980c5a5baf7a5
sahil dahake <sahildahake1999@gmail.com>
2022-10-12 09:35:45 +0530

    commit

---
+++ 10.c
@@ -0,0 +1,13 @@
+//while loop//
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int i=0;
+    while (i<20)
+    {
+        printf("%d\n", i);
+        i=i+1;
+    }
+    
+    return 0;
+}
---
+++ 9.c
@@ -0,0 +1,17 @@
+//do while loop
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int num, index=0;
+    printf("enter the number\n");
+    scanf("%d", &num);
+
+    do
+    {
+        printf("%d\n", index+1);
+        index = index +1;
+    } while (index<num);
+    
+
+    return 0;
+}
