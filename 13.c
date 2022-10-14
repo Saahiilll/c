9b8a5006604d617eac7efe2fbb66853cb6cf6cef
sahil dahake <sahildahake1999@gmail.com>
2022-10-14 09:46:20 +0530

    commit

---
+++ 13.c
@@ -0,0 +1,13 @@
+//variables//
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a=4;
+    float b=4.5;
+    char c='d';
+
+    printf("the value of a is %d\n", a);
+    printf("the value of b is %f\n",b);
+    printf("the value of c is %c\n",c);
+    return 0;
+}
