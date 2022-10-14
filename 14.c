e763c1755c6c3dd79bd964041601c684fe138019
sahil dahake <sahildahake1999@gmail.com>
2022-10-14 10:02:21 +0530

    commit

---
+++ 14.c
@@ -0,0 +1,14 @@
+//addition//
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a;
+    int b;
+    printf("enter the first value\n",a);
+    scanf("%d", &a);
+    printf("enter the second value\n",b);
+    scanf("%d",&b);
+
+    printf("the sum of this two values is %d",a+b);
+    return 0;
+}
