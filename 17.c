087db439be5058260bc5ed2f31a8c85d1edc6259
sahil dahake <sahildahake1999@gmail.com>
2022-10-16 12:28:45 +0530

    commit

---
+++ 17.c
@@ -0,0 +1,9 @@
+//calculate the area of circle
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int radius=2;
+    float pi=3.14;
+    printf("the area of the circle is %f",pi*radius*radius);
+    return 0;
+}
