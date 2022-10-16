cb69872e43c761e80c2b1ba65a1c262b33aa30ff
sahil dahake <sahildahake1999@gmail.com>
2022-10-16 12:46:25 +0530

    commit

---
+++ 19.c
@@ -0,0 +1,9 @@
+//write a program to convert celcius(centigrate degree to fareignite)
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    float celsius=37;
+    float far=(celsius * 9/5)+32;
+    printf("the value of this celsius temperature in farenheit is %f",far);
+    return 0;
+}
