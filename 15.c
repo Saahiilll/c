7ceb3a51e33d379052bbda79881a4341d586f3dc
sahil dahake <sahildahake1999@gmail.com>
2022-10-16 12:07:44 +0530

    commit

---
+++ 15.c
@@ -0,0 +1,12 @@
+// write a program to calculate a area of rectangle
+//using hard coded input
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int length=4,breadth=4;
+    int area=length*breadth;
+
+    printf("the area of the rectangle is %d\n",area);
+
+    return 0;
+}
