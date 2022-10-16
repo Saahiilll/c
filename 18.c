9d31f6f01efd57ac1fc8984070b318849991a712
sahil dahake <sahildahake1999@gmail.com>
2022-10-16 12:34:32 +0530

    commit

---
+++ 18.c
@@ -0,0 +1,11 @@
+//calculate the area of cylinder
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int radius=2;
+    float pi=3.14;
+    int height=3;
+     
+    printf("the area of the cylinder %f",pi*radius*radius*height);
+    return 0;
+}
