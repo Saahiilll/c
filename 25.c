31b48a18467786a7ea95a2cc15020d44ab933dea
sahil dahake <sahildahake1999@gmail.com>
2022-10-18 15:33:33 +0530

    commit

---
+++ 25.c
@@ -0,0 +1,11 @@
+//arithmatic operator
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a=4,b=8;
+    printf("the value of a+b is %d\n",a+b);
+    printf("the value of a-b is %d\n",a-b);
+    printf("the value of a*b is %d\n",a*b);
+    printf("the value of a/b is %d\n",a/b);
+    return 0;
+}
