65a1bbb15a1fb5c86eca2464171e63d07b2cc99d
sahil dahake <sahildahake1999@gmail.com>
2022-10-18 12:32:14 +0530

    commit

---
+++ 23.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a= 4, b ,c;
+    c=b=a;
+    printf("the value of a is %d\n",a);
+     printf("the value of b is %d\n",b);
+      printf("the value of c is %d\n",c);
+    return 0;
+}
