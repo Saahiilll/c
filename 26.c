3bcc293593f26780b7a6127ff6b1f8a501d0a5a5
sahil dahake <sahildahake1999@gmail.com>
2022-10-19 20:46:56 +0530

    commit

---
+++ 26.c
@@ -0,0 +1,9 @@
+//to find the exponential of the values
+#include<stdio.h>
+#include<math.h>
+int main(int argc, char const *argv[])
+{
+    printf("the value of the 2 to the power 3 is %f",pow(2,3));
+
+    return 0;
+}
