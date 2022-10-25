0f983b299ccb1893d7b473b786094087f64b92a3
sahil dahake <sahildahake1999@gmail.com>
2022-10-25 20:44:24 +0530

    commit

--- 37.c
+++ 37.c
@@ -5,6 +5,26 @@ int main(int argc, char const *argv[])
 {
     int year;
     printf("enter an year\n");
-    scanf("%d",&year)
+    scanf("%d",&year);
+
+    if (year % 400 == 0)
+    {
+        printf(" %d is the leap year\n",year);
+    }
+    if (year % 100 == 0)
+    {
+        printf(" %d is not a leap year\n",year);
+    }
+    if (year % 4 == 0)
+    { 
+        printf("%d is a leap year\n",year);
+    }
+    
+    else
+    {
+        ("the  %d is not a leap year\n",year);
+    }
+    
+    
     return 0;
 }
