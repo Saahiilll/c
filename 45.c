62eaf39f49daa7f266f32b47a9f83117c87d2a5a
sahil dahake <sahildahake1999@gmail.com>
2022-10-30 14:14:15 +0530

    commit

---
+++ 45.c
@@ -0,0 +1,9 @@
+//changing the element in an array//
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int mynumbers[]={1,2,3,4,5,6,7,8};
+    mynumbers[0]=43;
+    printf("%d",mynumbers[0]);
+    return 0;
+}
