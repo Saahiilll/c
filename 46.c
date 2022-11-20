fb81fb01595729e920bcbfe6f4ac6906821db8ad
sahil dahake <sahildahake1999@gmail.com>
2022-11-20 11:42:03 +0530

    commit

---
+++ 46.c
@@ -0,0 +1,11 @@
+//multiplication table
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{   int i;
+    for (i=10;i;i--)
+    {
+        printf("10*%d =%d\n",i,10*i);
+    }
+    
+    return 0;
+}
---
+++ 47.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int i;int sum=0;int n=10;
+    for (i = 0; i <=n; i++)
+    {
+       sum+=i;
+    }
+     printf("the sum of (1 to 10) is %d",sum);
+    
+    return 0;
+}
